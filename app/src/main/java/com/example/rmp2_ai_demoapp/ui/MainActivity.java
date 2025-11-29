edda0dc57fe78764810c356260dc7cab1710f348
Dm Ant <Dm@Ant.ru>
2025-11-29 14:03:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rmp2_ai_demoapp/ui/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rmp2_ai_demoapp.ui;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rmp2_ai_demoapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup genderGroup;
+    private TextView resultText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        genderGroup = findViewById(R.id.genderGroup);
+        resultText = findViewById(R.id.resultText);
+        sendButton = findViewById(R.id.sendButton);
+    }
+}
